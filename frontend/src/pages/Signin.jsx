7549b8ef0a25a0cf0ee201731c73c8804451098f
HeyAdin <adinahmad003@gmail.com>
2024-12-13 02:15:43 +0530

    added onclick function as per req and onchange function

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,18 +1,41 @@
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useRecoilState } from "recoil";
 import BelowButtonWarning from "../components/BelowButtonWarning";
 import ButtonComponent from "../components/ButtonComponent";
 import Heading from "../components/Heading";
 import Input from "../components/Input";
 import SubHeading from "../components/SubHeading";
+import { inputAtom } from "../atoms/inputs";
+import axios from "axios";
 export default function Signin() {
-    return <div style={{ backgroundColor: "white", height: "auto", width: "350px", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", margin: "auto", padding: "20px", borderRadius: "7px" }}>
+    const [userInput,setInput] =  useRecoilState(inputAtom);
+    const onClick = async()=>{
+        console.log(userInput);
+        const response = await axios.post("http://localhost:8001/api/v1/user/signin",userInput);
+        const data = response.data;
+        const token = "Bearer "+data.token;
+        localStorage.setItem("token", token);
+        console.log(data.msg)
+    }
+    return <div style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "gray",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center",
+        margin: "0px",
+        padding: "0px"
+    }}> 
+        <div style={{ backgroundColor: "white", height: "auto", width: "350px", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", margin: "auto", padding: "20px", borderRadius: "7px", textAlign : "center"}}>
         <RecoilRoot>
         <Heading title={'Sign In'} />
         <SubHeading subTitle={'Enter your information to Sign in to your account'} />
-        <Input type={'email'} lable={'Email'} id={"username"} placeholder={"johndoe@gmail.com"} />
-        <Input type={'password'} lable={'Password'} id={"password"} />
-        <ButtonComponent buttonTitle={"Sign In"} backgroundColor={"blueViolet"} fontSize={"24px"} route={"http://localhost:8001/api/v1/user/signin"}/>
+        <Input onChange={e => setInput({...userInput, username:e.target.value})} type={'email'} lable={'Email'} placeholder={"johndoe@gmail.com"} />
+        <Input onChange={e => setInput({...userInput, password:e.target.value})} type={'password'} lable={'Password'} />
+        <ButtonComponent onClick={onClick} buttonTitle={"Sign In"} backgroundColor={"blueViolet"} fontSize={"24px"}/>
         <BelowButtonWarning warning={"Don't have an account "} warningButton={<a href="/signup" style={{ color: "black", cursor: "pointer", textDecoration: "underline", fontWeight: "bold" }}>Sign up</a>} />
         </RecoilRoot>
+    </div>
     </div>
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,21 +1,45 @@
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useRecoilState } from "recoil";
 import BelowButtonWarning from "../components/BelowButtonWarning";
 import ButtonComponent from "../components/ButtonComponent";
 import Heading from "../components/Heading";
 import Input from "../components/Input";
 import SubHeading from "../components/SubHeading";
+import { inputAtom } from "../atoms/inputs";
+import axios from "axios"
 
-export default function Signup(){
-    return <div style={{backgroundColor:"white",height:"auto",width : "350px", boxShadow :"rgba(0, 0, 0, 0.35) 0px 5px 15px" ,  margin:"auto", padding : "20px", borderRadius:"7px"}}>
-        <Heading title={'Sign Up'} />
-        <SubHeading subTitle={'Enter your information to create your account'} />
-        <RecoilRoot>
-        <Input type={'text'} lable={'First Name'} id={"firstName"} placeholder={"John"}/>
-        <Input type={'text'} lable={'Last Name'} id={"lastName"} placeholder={"Doe"}/>
-        <Input type={'email'} lable={'Email'} id={"username"} placeholder={"johndoe@gmail.com"}/>
-        <Input type={'password'} lable={'Password'} id={"password"} />
-        <ButtonComponent buttonTitle={"Sign Up"} backgroundColor={"blueViolet"} fontSize={"24px"} route={"http://localhost:8001/api/v1/user/signup"}/>
-        </RecoilRoot>
-        <BelowButtonWarning warning={"Already have an account "} warningButton={<a href="/signin" style={{ color:"black", cursor:"pointer" ,textDecoration:"underline", fontWeight:"bold"}}>Sign in</a>} />
+export default function Signup() {
+    const [userInput,setInput] =  useRecoilState(inputAtom);
+
+    const onClick = async()=>{
+        console.log(userInput);
+        const response = await axios.post("http://localhost:8001/api/v1/user/signup",userInput);
+        const data = response.data;
+        const token = "Bearer "+data.token;
+        localStorage.setItem("token", token);
+        console.log(data.msg);
+    }
+    return <div style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "gray",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center",
+        margin: "0px",
+        padding: "0px"
+    }}>
+        <div style={{ backgroundColor: "white", height: "auto", width: "350px", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", margin: "auto", padding: "20px", borderRadius: "7px", textAlign: "center" }}>
+            <Heading title={'Sign Up'} />
+            <SubHeading subTitle={'Enter your information to create your account'} />
+            <RecoilRoot>
+                <Input onChange={e => setInput({...userInput, firstName:e.target.value})} type={'text'} lable={'First Name'} placeholder={"John"} />
+                <Input onChange={e => setInput({...userInput, lastName:e.target.value})} type={'text'} lable={'Last Name'} placeholder={"Doe"} />
+                <Input onChange={e => setInput({...userInput, username:e.target.value})} type={'email'} lable={'Email'} placeholder={"johndoe@gmail.com"} />
+                <Input onChange={e => setInput({...userInput, password:e.target.value})} type={'password'} lable={'Password'} />
+                <ButtonComponent onClick={onClick} buttonTitle={"Sign Up"} backgroundColor={"blueViolet"} fontSize={"24px"}/>
+            </RecoilRoot>
+            <BelowButtonWarning warning={"Already have an account "} warningButton={<a href="/signin" style={{ color: "black", cursor: "pointer", textDecoration: "underline", fontWeight: "bold" }}>Sign in</a>} />
+        </div>
     </div>
 }
